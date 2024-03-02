9c2daae64eacfb7de9a81513279711f90b7ae9fc
Igor <igornteixeira48@gmail.com>
2024-03-02 02:16:26 -0300

    start

---
+++ src/components/home/home.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+    <div className="nav">
+      <h1>Igor Nunes Teixeira</h1>
+      <a href="projetos">Projetos</a>
+    </div>
+    </>
+  );
+}
